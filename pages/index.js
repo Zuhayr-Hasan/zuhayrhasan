30626e4eeb4d2edba09ef36b53d65fab2e015b50
Zuhayr Hasan <laptopmart@Zuhayrs-Macbook-Pro.local>
2024-08-16 03:46:25 +0500

    third-commit

--- pages/index.js
+++ pages/index.js
@@ -91,10 +91,11 @@ export default function Home() {
             </h1>
           </div>
 
-          <Socials className="mt-2 laptop:mt-5" />
+          {/* <Socials className="mt-2 laptop:mt-5" /> */}
+          <Button type="secondary" onClick={() => window.open("https://calendly.com/zuhayrhasandev/discussion-on-project")}>Book Free Consultation</Button>
         </div>
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
-          <h1 className="text-2xl text-bold">Work.</h1>
+          <h1 className="text-2xl text-bold">Projects.</h1>
 
           <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
             {data.projects.map((project) => (
